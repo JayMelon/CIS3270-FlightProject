a5e303ce04a3247fb548f4b14341d333189539ae
Jackson Jones <63122233+JacksonKJones@users.noreply.github.com>
2022-12-05 01:17:14 -0500

    Class correction

--- Final Term Project - CIS3270/src/gui/Screen.java
+++ Final Term Project - CIS3270/src/gui/Screen.java
@@ -1,47 +1,27 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 
 public class Screen extends Application {
 	
-	Button button;
-	Stage window;
-	Scene scene1, scene2;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		window = primaryStage;
-		
-		Label label1 = new Label("Welcome to Scene 1");
-		Button button1 = new Button("Go to Scene 2");
-		button1.setOnAction(e -> window.setScene(scene2));
-		
-		VBox layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1, button1);
-		scene1 = new Scene(layout1, 200, 200);
-		
-		Button button2 = new Button("Go to Scene 1");
-		button2.setOnAction(e -> window.setScene(scene1));
-		
-		StackPane layout2 = new StackPane();
-		layout2.getChildren().add(button2);
-		scene2 = new Scene(layout2, 600, 300);
-		
-		window.setScene(scene1);
-		window.setTitle("Title here");
-		window.show();	
-	}
-
 }
