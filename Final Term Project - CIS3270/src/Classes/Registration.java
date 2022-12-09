411752ad8b167321b91186828d18739aafb5f50b
Jay <Jay@ThunderRod>
2022-12-09 00:09:40 -0500

    Cleaned out classes and methods, Basic Login function.

---
+++ Final Term Project - CIS3270/src/Classes/Registration.java
@@ -0,0 +1,16 @@
+package Classes;
+
+import java.sql.*;
+
+import Database.FlightDatabase;
+
+public class Registration extends User {
+
+	
+//Going to create a method that registers people to database. 	
+	/*public void registerUser() {
+		Connection con = FlightDatabase.getConnect();
+		String insertFields = "INSERT INTO "+User.databaseName+" "
+	}
+	*/
+}
