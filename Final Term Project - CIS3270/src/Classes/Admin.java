180fb2e707ad39a92f2f26566f8fe8a3e228c7a9
Jay <Jay@ThunderRod>
2022-12-11 14:25:14 -0500

    Admin class, Added checkAdmin

--- Final Term Project - CIS3270/src/Classes/Admin.java
+++ Final Term Project - CIS3270/src/Classes/Admin.java
@@ -2,5 +2,14 @@ package Classes;
 
 public class Admin extends User  {
 
+	//Check if User is admin
+public boolean checkAdmin() {
+	if((this.userID).equals("A")) {
+		return true;
+	}else {
+		return false;
+	}
+		
+}
 }
 
