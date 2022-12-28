a3b3dc1eab5477991f433d4d4676caabc4b1c992
Jay <Jay@ThunderRod>
2022-12-27 22:52:15 -0500

    Not able to load UserFlights Page

--- Final Term Project - CIS3270/src/gui/UserFlightsController.java
+++ Final Term Project - CIS3270/src/gui/UserFlightsController.java
@@ -3,32 +3,80 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import Classes.Flight;
+import Classes.FlightController;
+import Classes.Reservation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-public class UserFlightsController {
-	
+public class UserFlightsController implements Initializable{
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 	@FXML
 	private Button deleteFlightButton;
 	
 	@FXML
-	private ListView<String> userFlightsListView;
+	private TableView<Flight> userFlightsTableView;
+	@FXML
+	private TableColumn<Flight, String> flightFromCityCol;
+	@FXML
+	private TableColumn<Flight, String> flightToCityCol;
+	@FXML
+	private TableColumn<Flight, String> flightDepartTimeCol;
+	@FXML
+	private TableColumn<Flight, String> flightArrivalTimeCol;
+	@FXML
+	private TableColumn<Flight, String> flightDepartDateCol;
+	@FXML
+	private TableColumn<Flight, String> flightArrivalDateCol;
+
 	
-	private String[] userFlights = {};
-	
-	public void initialize(URL url, ResourceBundle resourceBundle) {
-		userFlightsListView.getItems().addAll(userFlights);
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg) {
+		try {
+			
+			
+		} catch (Exception e) {
+			Logger.getLogger(UserFlightsController.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		}
+			/*new TableView<Flight>();
+			ObservableList<Flight> flightObservableList = FXCollections.observableArrayList();
+			Reservation userFlights = new Reservation(Main.userID);
+			userFlights.getUserFlightIDs();
+			userFlights.getUserFlightData();
+			flightObservableList.addAll(userFlights.userFlightData);
+			flightFromCityCodeCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("FromCityCode"));
+			flightToCityCodeCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("ToCityCode"));
+			flightDepartTimeCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("departTime"));
+			flightArrivalTimeCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("arrivalTime"));
+			flightDepartDateCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("departDate"));
+			flightArrivalDateCol.setCellValueFactory(new PropertyValueFactory<Flight, String>("arrivalDate"));
+			userFlightsTableView.setItems(flightObservableList);
+			userFlightsTableView.refresh();
+			*/
 	}
-	
 	public void deleteUserFlight(ActionEvent event) throws IOException {
 		//if(userFlightsListView.getValue() = "") {
 		//	Alert alert = new Alert(AlertType.CONFIRMATION);
@@ -46,9 +94,6 @@ public class UserFlightsController {
 		//	System.out.println(Main.userType + Main.user + " did not select a flight booking to delete!");
 	}
 	
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
 	
 	// sends the user to the main menu
 	public void switchToMainMenu(ActionEvent event) throws IOException {
